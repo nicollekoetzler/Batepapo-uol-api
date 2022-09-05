8a9db65d9ea33c9418baec0c865d99fec82eb7cd
nicollekoetzler <nickkoetzler@gmail.com>
2022-09-05 15:46:38 -0300

    valida participantes do chat

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, {json} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
+import joi from "joi";
 
 dotenv.config()
 
@@ -13,9 +14,38 @@ db = mongoClient.connect().then( () => {
 
 const server = express();
 server.use(cors());
+server.use(json());
 
-server.get("/teste",(req,res) => {
-    res.status(200).send("deu bom");
+// formato participante: {name: 'João', lastStatus: 12313123}
+// formato mensagem: {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+
+server.post("/participants", async (req, res) => {
+
+    const userData = req.body
+    const userDataSchema = joi.object(
+        {
+          name: joi.string().required()
+        }
+      );
+    const isBodyValid = userDataSchema.validate(userData, { abortEarly: false })
+
+
+    if (isBodyValid.error) {
+        return res.sendStatus(422);
+    }
+
+    try{
+        const isRegistered = await db.collection("participants").findOne(userData)
+        
+        if (isRegistered){
+            return res.sendStatus(409);
+        }
+
+        res.sendStatus(201);
+    }catch{
+        res.sendStatus(500);
+    }
+
 })
 
 server.listen(process.env.PORT, () => console.log("servidor rodando"));
